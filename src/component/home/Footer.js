9000a1649ae991f43b1368cde7eca2106993acfa
sopranoelfuego <aubinjaja@gmail.com>
2021-04-09 23:30:29 +0200

    signature

--- src/component/home/Footer.js
+++ src/component/home/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className="footer">
-           <h6> &copy;{new Date().getFullYear()} gd-experts all right-reserved<span>designed by <address>aubinjaja@gmail.com</address></span></h6>
+           <h6> &copy;{new Date().getFullYear()} gd-experts all right-reserved<span> designed by <address>aubinjaja@gmail.com</address></span></h6>
         </div>
     )
 }
